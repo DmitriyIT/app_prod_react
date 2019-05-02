1e6189471b7f48d1c68d5850115cd34ccdfbfe2c
Dmitriy Baranov <mit1035@yandex.ru>
2019-05-02 17:48:38 +0300

    del bef and ann app requests

--- app.js
+++ app.js
@@ -11,38 +11,191 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/main', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-app.get('/startups', function(req, res) {
-	var more_info = {
-		author_img: 'people.svg', 
-		author_name: 'Вася Вася',
-		need_people: [
-			{
-				position: 'программист JS',
-				isFound: true
-			},
-			{
-				position: 'Маркетолог',
-				isFound: false
-			},
-			{
-				position: 'Дизайнер',
-				isFound: true
+app.post('/checkEmail', function(req, res) {
+	console.log(req.body);
+	var ans = (req.body.email === "awd");
+	res.json({ ans: ans });
+});
+
+
+app.post('/login', function(req, res) {
+	console.log(req.body);
+	var response;
+	if (req.body.email == 'awd') {
+		response = {
+			code: 21,
+			userData:{
+				name:"strawd",
+				surName:"str",
+				photo:"str",
+				description:"",
+				notifications:[],
+				myStartupId: 12
 			}
-		]
-	};
-	var startups = [
-		{title: 'Площадка для ddddстартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 3, more_info: more_info},
-		{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 4, more_info: more_info},
-		{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 5, more_info: more_info},
-		{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 6, more_info: more_info},
-		{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 7, more_info: more_info},
-		{title: 'Площадка для стартаdwadwв', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 8, more_info: more_info}
+		}
+	} else {
+		response = false;
+	}
+	res.json(response);
+});
+
+app.post('/reg', function(req, res) {
+	console.log(req.body);
+	var response;
+	if (req.body.email == 'awd') {
+		response = {
+			code: 21,
+			userData:{
+				name:"strawd",
+				surName:"str",
+				photo:"str",
+				description:"",
+				notifications:[],
+				myStartupId: 12
+			}
+		}
+	} else {
+		response = false;
+	}
+	res.json(response);
+});
+
+app.post('/createstartup', function(req, res) {
+	console.log(req.body);
+	res.json({ code: 21 });
+});
+
+app.get('/getInfoOfStartup', function(req, res) {
+	var fields = [
+		{name: 'theme',        value: 'Площадка для стартапов'},
+		{name: 'description',  value: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их. И еще текст тест'},
+		{name: 'shortDescr',   value: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их'},
+		{name: 'peopleNeeded', value: 'программист JS Маркетолог Дизайнер'},
+		{name: 'contacts',     value: 'some mail'}
 	];
-   res.json(startups);
+   res.json(fields);
+});
+app.get('/getInfoOfStartup1', function(req, res) {
+	var fields = {
+		'theme'        : 'Площадка для стартапов',
+		'description'  : 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их. И еще текст тест',
+		'shortDescr'   : 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их',
+		'peopleNeeded' : 'программист JS; Маркетолог; Дизайнер',
+		'contacts'     : 'some mail'
+	}
+   res.json(fields);
+});
+
+app.get('/mystartup', function(req, res) {
+	var mystartup = {
+		role: "master", //master||member||no
+		startup:{
+			title: 'Площадка для стартапов',
+			description: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их. И еще текст тест',
+			messages:[],
+			members:[],
+			startup_requests:[] //if role == master
+		}
+	};
+   res.json(mystartup);
+});
+
+var more_info = {
+	author_img: 'people.svg', 
+	author_name: 'Вася Вася',
+	need_people: [
+		{
+			position: 'программист JS',
+			isFound: true
+		},
+		{
+			position: 'Маркетолог',
+			isFound: false
+		},
+		{
+			position: 'Дизайнер',
+			isFound: true
+		}
+	]
+};
+var startups = [
+	{title: 'Площадка для ddddстартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 3, more_info: more_info},
+	{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 4, more_info: more_info},
+	{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 5, more_info: more_info},
+	{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 6, more_info: more_info},
+	{title: 'Площадка для стартапов', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 7, more_info: more_info},
+	{title: 'Площадка для стартаdwadwв', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 8, more_info: more_info}
+];
+app.post('/findStartups', function(req, res) {
+	var res_sent = startups;
+	if (req.body.find_str == "awd") {
+		res_sent = [{title: 'ответ на awd str_search ;)', body: 'Создать сайт, где люди смогут обхединяться в стартапы и создавать их.', id: 4, more_info: more_info}];
+	}
+	console.log(req.body);
+	res.json(res_sent);
+});
+
+
+var messages = [
+    {
+        author_icon_path: '../../img/icon/vasia.jpg',
+        author_name: 'Вася Васькин',
+        time: '122',
+        text: 'что то в сообщении'
+    },
+    {
+        author_icon_path: '../../img/icon/vasia.jpg',
+        author_name: 'Вася Васькин2',
+        time: '12',
+        text: 'что то в сообщении2'
+    }
+];
+app.get('/getMsgs', function(req, res) {
+   res.json(messages);
+});
+app.post('/sendMsg', function(req, res) {
+	// console.log(req.body);
+	// messages.push(req.body.text);
+	res.json(messages);
+});
+
+app.get('/getMembers', function(req, res) {
+	var members = [
+		{name: 'Вася Грингки', job: 'js программист', img_src: '../../img/icon/vasia.jpg', id: 21, show_popup: false},
+		{name: 'Вася Грингки2', job: 'маркетолог', img_src: '../../img/icon/vasia.jpg', id: 31, show_popup: false},
+		{name: 'Вася Грингки3', job: 'дизайнер', img_src: '../../img/icon/vasia.jpg', id: 41, show_popup: false}
+	];
+	var isAdmin = true;
+   res.json({
+   	members: members,
+   	isAdmin: isAdmin
+   });
+});
+app.post('/delMember', function(req, res) {
+	console.log(req.body.id);
+	// messages.push(req.body.text);
+	res.json({ans: true});
+});
+
+app.get('/getInvites', function(req, res) {
+	var jobs = ['js программист', 'маркетолог', 'дизайнер'];
+	var invites = [
+		{name: 'Вася Грингки1', jobList: jobs, img_src: '../../img/icon/vasia.jpg', id: 21},
+		{name: 'Вася Грингки2', jobList: jobs, img_src: '../../img/icon/vasia.jpg', id: 31},
+		{name: 'Вася Грингки3', jobList: jobs, img_src: '../../img/icon/vasia.jpg', id: 41}
+	];
+   res.json({
+   	invites: invites
+   });
+});
+app.post('/ansOfInvite', function(req, res) {
+	console.log('id = ' + req.body.id + ' ans = ' + req.body.answer + ' job = ' + req.body.job);
+	// messages.push(req.body.text);
+	res.json({name: 'Вася Грингки', job: req.body.job});
 });
 
 // app.use('/', indexRouter);
